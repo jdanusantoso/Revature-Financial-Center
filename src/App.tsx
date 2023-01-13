4bcf1a74d05715f69cb8aecf9c141215d5850b1b
Fatimat <fsheleru@gmail.com>
2023-01-13 14:40:52 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Components/Login/login';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <BrowserRouter>
+    <Routes>
+      <Route path='' element ={<Login/>}/>
+    </Routes>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
