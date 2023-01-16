c6a2299d9083656700d378ce77205f24375797cc
jdanusantoso <jdanusantoso@hotmail.com>
2023-01-15 23:05:46 -0600

    set up userDao and Controller, can register

---
+++ src/main/java/com/revature/daos/UserDAO.java
@@ -0,0 +1,17 @@
+package com.revature.daos;
+
+import com.revature.models.Transactions;
+import com.revature.models.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDAO extends JpaRepository<Users, Integer> {
+
+    //This is our entire DAO for now... showing custom DAO method below
+
+    public Optional<List<Users>> getByUsernameAndPassword(String username, String passsword);
+
+
+}
