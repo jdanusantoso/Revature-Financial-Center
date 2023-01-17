767950f824e0806597a957490096ff98eb3e0a73
jdanusantoso <jdanusantoso@hotmail.com>
2023-01-16 23:00:10 -0600

    backend transaction methods

--- src/main/java/com/revature/controllers/TransactionsController.java
+++ src/main/java/com/revature/controllers/TransactionsController.java
@@ -4,13 +4,17 @@ import com.revature.daos.AccountsDAO;
 import com.revature.daos.TransactionsDAO;
 import com.revature.models.Accounts;
 import com.revature.models.Transactions;
+import com.revature.models.Users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController //This combines @Controller and @ResponseBody
 @CrossOrigin //lets us take in HTTP requests (might have to specify ports in your P2)
-@RequestMapping(value="/accounts")
+@RequestMapping(value="/transactions")
 public class TransactionsController {
 
     //autowiring the DigimonDAO with constructor injection
@@ -45,5 +49,37 @@ public class TransactionsController {
         return ResponseEntity.accepted().body(newTransaction); //send a 202 status code and the new digimon
     }
 
+    @GetMapping (value="/getAllTransactions")
+    //url: localhost:5556/data/accounts/getAllAccounts
+    public ResponseEntity<List<Transactions>> getAllAccounts(){
+
+        return ResponseEntity.ok(tDAO.findAll()); //.ok() returns a 200 level status code
+
+        //note that I'm nesting the DB call in the actual return
+        //this is shorthand - I could have done .ok().body(dDAO.findAll());
+
+    }
+
+    @GetMapping (value="/getAllTransactionType")
+    //url: localhost:5556/data/transactions/getAllTransactionType?accountIdTransaction=1&transactionType=expense
+    //url: localhost:5556/data/transactions/getAllTransactionType?accountIdTransaction=1&transactionType=income
+    public ResponseEntity<List<Transactions>> getByAcctIdAndTransactionType(@RequestParam int accountIdTransaction, @RequestParam String transactionType){
+
+        Optional<List<Transactions>> transactionOptional = tDAO.getByAccountIdTransactionAndTransactionType(accountIdTransaction, transactionType );
+
+        //we can check if the optional has data with .isPresent(), or .isEmpty()
+        if(transactionOptional.isPresent()){
+            //we can extract the Optional's data with .get()
+            List<Transactions> extractedTransaction = transactionOptional.get();
+
+            return ResponseEntity.ok(extractedTransaction);
+        }
+        //if get by name failed...
+        return ResponseEntity.badRequest().build(); //returning a 400 with no response body
+
+    }
+
 
 }
+
+
